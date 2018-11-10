07a3ea9728bc0f13dada972c015d71dc3d03dfc8
null096 <33376471+null096@users.noreply.github.com>
2018-11-10 18:33:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Treemap } from 'react-vis';
+
+function getRandomData(total) {
+  const totalLeaves = total || 400;
+  const children = [];
+  for (let i = 0; i < totalLeaves; i++) {
+    children.push({
+      name: total ? total : String(Math.random()).slice(0, 3),
+      size: Math.random() * 1000,
+      color: Math.random(),
+    });
+  }
+  return {
+    title: '',
+    color: 2,
+    children
+  };
+}
 
 class App extends Component {
   render() {
+    const treeProps = {
+      mode: 'circlePack',
+      renderMode: 'SVG',
+      width: 700,
+      height: 600,
+      animation: {
+        damping: 16,
+        stiffness: 80,
+      },
+      style: {
+        stroke: 'rgb(148, 36, 192)',
+      },
+      colorDomain: [0, 1, 2],
+      colorRange: ['white', 'black', 'gray'],
+      padding: 2,
+      data: getRandomData(),
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <button onClick={() => this.forceUpdate()}>Update</button>
+        <Treemap {...treeProps} />
+      </React.Fragment>
     );
   }
 }
