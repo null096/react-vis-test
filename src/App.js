bf8f4fc39faf8972713987758abbc4e331e083a0
null096 <33376471+null096@users.noreply.github.com>
2018-11-10 23:02:36 +0200

    web socket added /beta

--- src/App.js
+++ src/App.js
@@ -18,8 +18,52 @@ function getRandomData(total) {
   };
 }
 
+let data = getRandomData();
+
 class App extends Component {
+  state = {
+    socketStatus: 'Not connected',
+    countOfTransactions: 0,
+  }
+
+  connect = () => {
+    this.ws = new WebSocket('wss://ws.blockchain.info/inv');
+    this.ws.onopen = () => {
+      this.ws.send('{"op":"unconfirmed_sub"}');   // TODO rewrite
+      this.setState({ socketStatus: 'Connected' });
+    };
+    this.ws.onerror = () => { this.setState({ socketStatus: 'Error' }); };
+    this.ws.onclose = () => { this.setState({ socketStatus: 'Disconnected' }); };
+    this.ws.onmessage = this.onWebSocketMessage;
+  }
+
+  disconnect = () => {
+    this.ws.send('{"op":"unconfirmed_unsub"}');
+    this.ws.close();
+  }
+
+  onComponentUnmount() {
+    this.disconnect();
+  }
+
+  onWebSocketMessage = (e) => {
+    const data = JSON.parse(e.data);
+    const x = 10 ** 8;
+
+    console.log(e, data);
+    data.x.inputs.forEach(e => console.log(e.prev_out.value / x));
+    data.x.out.forEach(e => console.log(e.value / x));
+    this.setState((prevState) => ({
+      countOfTransactions:
+        prevState.countOfTransactions + 1
+    }));
+  }
+
   render() {
+    if (data.children.length > 400) {
+      data.children.shift();
+    }
+    data.children.push(getRandomData(1).children[0]);
     const treeProps = {
       mode: 'circlePack',
       renderMode: 'SVG',
@@ -30,18 +74,22 @@ class App extends Component {
         stiffness: 80,
       },
       style: {
-        stroke: 'rgb(148, 36, 192)',
+        // stroke: 'rgb(148, 36, 192)',
       },
       colorDomain: [0, 1, 2],
       colorRange: ['white', 'black', 'gray'],
       padding: 2,
-      data: getRandomData(),
+      data
     };
 
     return (
       <React.Fragment>
         <button onClick={() => this.forceUpdate()}>Update</button>
-        <Treemap {...treeProps} />
+        <button onClick={this.connect}>Connect</button>
+        <button onClick={this.disconnect}>Disconnect</button>
+        <span>{this.state.socketStatus}</span>
+        <p>{this.state.countOfTransactions}</p>
+        {/* <Treemap {...treeProps} /> */}
       </React.Fragment>
     );
   }
